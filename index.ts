7876a1cfbcc376a9ee936b524f2d7e44e2a410c1
farahmuzaffar <164481851+farahmuzaffar@users.noreply.github.com>
2024-04-01 21:16:11 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// computer will generate a random number
+// user input for guessing number
+// compare user input with computer generated number
+
+const randomAlphabet = Math.floor(Math.random()* 6 + 1);
+
+
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessAlphabet",
+        type: "string",
+        message: "please guess an alphabet between a-f:",
+
+}])
+if(answers.userGuessAlphabet === randomAlphabet){
+    console.log("congratulations! you guessed right !");
+}else{
+    console.log("you guessed wrong !");
+}
